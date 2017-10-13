3984b5d5e43f0c4abcd214d4dd76e699c64af2fd
ThomasKar <ThomasKar@users.noreply.github.com>
2017-10-13 13:19:29 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 
 document.getElementById("clickMe").innerHTML
 
-function change(){
-	document.getElementById("myParagraph").innerHTML = "Replace by this text!";
+function addEmpireText(theId, text){
+	document.getElementById(theId).innerText = text;
 }
+
+addEmpireText("myDiv", "The Galactic Empire Series\r\n\r\n\r\nThese stories are set in the same future as the Foundation series, which had appeared in magazines starting in 1942. The tie is not close, and they are only loosely connected to each other, each being a complete tale in its own right. Their main common points are Asimov\'s idea of a future Galactic Empire, certain aspects of technology \u2014 hyperdrive, blaster pistols, \"neuronic whips\", the possible invention of the \"Visi-Sonor\" \u2014 and particular locations, such as the planet Trantor. Another connection was later established with Robots and Empire, where Asimov revealed how Earth became radioactive, as mentioned in all three novels. Some sources further this argument by asserting that The Stars, Like Dust takes place about one thousand years following the events of Robots and Empire.[3] Also, the calendar used on spaceships in The Stars, Like Dust is the same that the Spacers introduce Lije Baley to in The Robots of Dawn.\r\n\r\nThe short story \"Blind Alley\" is the only story set in the Foundation universe to feature intelligence not of human origin; Foundation and Earth features non-human intelligences (of Solaria and Gaia), but they are descended from or created by humans.\r\n\r\nAsimov later integrated them into his all-engulfing Foundation series. Some contortion was required to explain how the robots of the Robot series are almost completely absent from the Galactic Empire novels. In reality, this is because Asimov wrote the original Robot and Foundation short stories as separate series.");
 
 /* if (5<4){
 	console.log("It is true.");
