c95c5d3c83cc49ff6defca00bd77100b257abd99
ThomasKar <ThomasKar@users.noreply.github.com>
2017-10-13 11:31:04 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+
+document.getElementById("clickMe").innerHTML
+
+function change(){
+	document.getElementById("myParagraph").innerHTML = "Replace by this text!";
+}
+
+/* if (5<4){
+	console.log("It is true.");
+}
+else{
+	console.log("It is false!");
+}
+
+function printApples(nrApples) {
+	while(nrApples<15){
+		console.log("I have an apple");
+		nrApples+=1;
+	}
+}
+
+printApples(10);
+
+console.log("hello");
+alert("Watch Out!!!!");
+console.log(0.2345*1000);
+console.log(2+5);
+console.log(7/8);
+console.log(Math.random());
+
+document.getElementById('pop').innerHTML = "Hola, Dis Works Dude... Super Cool!";*/
